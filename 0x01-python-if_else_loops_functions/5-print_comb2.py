848b150aa4445dd1df19e80d94abc5ffc0e59cd7
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-09-11 03:32:43 +0100

    completed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for k in range(100):
     if k != 99:
-        print("{:00}".format(k), end=", ")
+        print("{:02}".format(k), end=", ")
     else:
-        print("{:00}".format(k))
+        print("{:01}".format(k))
