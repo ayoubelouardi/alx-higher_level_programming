36b3072f52e1c9fc6035201de204baaad7ba46e3
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-09-15 19:24:27 +0100

    completed

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+argc = len(sys.argv) - 1
+
+if argc == 0:
+    print("{} arguments.".format(argc))
+else:
+    print("{} arguments:".format(argc))
+for i in range(1, argc + 1):
+    print("{}: {}".format(i, sys.argv[i]))
