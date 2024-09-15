ee7ead67d67e49ec447015c7a3daaf573c57e87b
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-09-15 18:50:02 +0100

    completed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import *
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
+
 
 a = 10
 b = 5
