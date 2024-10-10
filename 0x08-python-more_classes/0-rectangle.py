56323f9799e660c8f4495efabc6293065665b756
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2024-10-10 02:22:15 +0100

    wow this is not what i expected

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Rectangle
+this is a Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Empty representation of a rectangle
+    this is a Rectangle
+    """
+    pass
