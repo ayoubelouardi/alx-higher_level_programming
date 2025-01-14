7b9761678415768de0e28b54f68ec322e38f7ad1
Ayoub El Ouardi <ayoubelouardioff@gmail.com>
2025-01-14 16:43:00 +0100

    completed

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
-"""adds the State object “California”
-with the City “San Francisco”
-to the database hbtn_0e_100_usa"""
+"""
+Adds the State object "California" with the City "San Francisco" to the
+database.
+
+This script connects to a MySQL database and creates a new `State` object
+named "California" and a new `City` object named "San Francisco". It then
+establishes a relationship between the two and commits them to the database.
+"""
+
+import sys
+from sqlalchemy.exc import SQLAlchemyError
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
 
 if __name__ == "__main__":
+    try:
+        # Create a connection to the database
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1],  # MySQL username
+                sys.argv[2],  # MySQL password
+                sys.argv[3]   # Database name
+            ),
+            pool_pre_ping=True
+        )
+        Base.metadata.create_all(engine)
 
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    new_city = City(name='San Francisco')
-    new = State(name='California')
-    new.cities.append(new_city)
-    session.add_all([new, new_city])
-    session.commit()
-    session.close()
+        # Create a session and add the new State and City objects
+        session = Session(engine)
+        new_city = City(name='San Francisco')
+        new_state = State(name='California')
+        new_state.cities.append(new_city)
+        session.add_all([new_state, new_city])
+        session.commit()
+    except SQLAlchemyError as e:
+        print(f"An error occurred: {e}")
+    finally:
+        session.close()
